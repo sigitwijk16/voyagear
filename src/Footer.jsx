8c2f4f902f27a7bf276fee5f69f546f9b687e521
sigitwijk16 <107915024+sigitwijk16@users.noreply.github.com>
2024-05-09 08:01:17 +0700

    revised footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 mt-8">
-      <div className="container mx-auto grid grid-cols-2 gap-4 justify-items-center">
+    <footer className="bg-gray-800 text-white py-8 mt-8 px-6 md:px-0">
+      <div className="container mx-auto grid grid-cols-2 justify-items-center">
         <div className="text-left">
-          <h2 className="text-xl font-semibold">Column 1</h2>
+          <h2 className="text-base lg:text-xl font-semibold">Column 1</h2>
           <p>This is the content of column 1.</p>
           <p>This is the content of column 1.</p>
           <p>This is the content of column 1.</p>
         </div>
         <div className="text-left">
-          <h2 className="text-xl font-semibold">Column 2</h2>
+          <h2 className="text-base lg:text-xl font-semibold">Column 2</h2>
           <p>This is the content of column 2.</p>
           <p>This is the content of column 2.</p>
           <p>This is the content of column 2.</p>
