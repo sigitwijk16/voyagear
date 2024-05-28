afef86a0e33c242e5e451336aedcf492e529535c
sigitwijk16 <107915024+sigitwijk16@users.noreply.github.com>
2024-05-28 19:39:41 +0700

    added button icons

--- src/MenuButtons.jsx
+++ src/MenuButtons.jsx
@@ -2,22 +2,46 @@ import React from "react";
 
 const MenuButtons = () => {
   return (
-    <div className="grid grid-cols-4 mx-8 md:mx-64 mt-12">
-      <div className="flex flex-col items-center col-span-1">
-        <img src="./images/telephone.png" className="w-10 md:w-16" alt="" />
+    <div className="grid grid-cols-4 mx-20 md:mx-72 mt-12 gap-4">
+      <div className="flex flex-col items-center col-span-2 md:col-span-1">
+        <div className="px-4 md:px-2 py-4 rounded-xl border-2 border-black">
+          <img
+            src="./buttons/montir.png"
+            className="w-10 md:w-16"
+            alt="Panggil Montir"
+          />
+        </div>
         <p className="text-xs md:text-base mt-2 text-center">Panggil Montir</p>
       </div>
-      <div className="flex flex-col items-center col-span-1">
-        <img src="./images/telephone.png" className="w-10 md:w-16" alt="" />
+      <div className="flex flex-col items-center col-span-2 md:col-span-1">
+        <div className="px-4 md:px-2 py-4 rounded-xl border-2 border-black">
+          <img
+            src="./buttons/service.png"
+            className="w-10 md:w-16"
+            alt="Service"
+          />
+        </div>
         <p className="text-xs md:text-base mt-2 text-center">Service</p>
       </div>
-      <div className="flex flex-col items-center col-span-1">
-        <img src="./images/telephone.png" className="w-10 md:w-16" alt="" />
+      <div className="flex flex-col items-center col-span-2 md:col-span-1">
+        <div className="px-4 md:px-2 py-4 rounded-xl border-2 border-black">
+          <img
+            src="./buttons/derek.png"
+            className="w-10 md:w-16"
+            alt="Panggil Derek"
+          />
+        </div>
         <p className="text-xs md:text-base mt-2 text-center">Panggil Derek</p>
       </div>
-      <div className="flex flex-col items-center col-span-1">
-        <img src="./images/telephone.png" className="w-10 md:w-16" alt="" />
-        <p className="text-xs md:text-base  mt-2 text-center">Sparepart</p>
+      <div className="flex flex-col items-center col-span-2 md:col-span-1">
+        <div className="px-4 md:px-2 py-4 rounded-xl border-2 border-black">
+          <img
+            src="./buttons/sparepart.png"
+            className="w-10 md:w-16"
+            alt="Sparepart"
+          />
+        </div>
+        <p className="text-xs md:text-base mt-2 text-center">Sparepart</p>
       </div>
     </div>
   );
